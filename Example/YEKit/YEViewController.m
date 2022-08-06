cd232ac2ad60c97737a99522f371e12e8584550d
yongen <280798744@qq.com>
2022-08-06 15:57:35 +0800

    YEKit第一次提交

--- Example/YEKit/YEViewController.m
+++ Example/YEKit/YEViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "YEViewController.h"
+#import "YEKit/YESandboxSave.h"
 
 @interface YEViewController ()
 
@@ -18,6 +19,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    YESandboxSave *save = [[YESandboxSave alloc]init];
+    [save sandboxSave];
 }
 
 - (void)didReceiveMemoryWarning
